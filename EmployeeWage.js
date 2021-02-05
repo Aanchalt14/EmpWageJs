5c62340756b2ced9d4d41c49e3ba24b63c88172b
Aanchalt14 <aanchalt71@gmail.com>
2021-02-06 00:57:36 +0530

    Add : Calculate Daily Wage of Employee

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,19 @@
 console.log('Welcome to EmployeeWage Computation');
-const IS_ABSENT =0
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if ( empCheck == IS_ABSENT) {
-    console.log("Employee is absent");
+
+const WAGE_PER_HOUR=20;
+
+let empHrs=0;
+let empCheck=Math.floor(Math.random()*10)%3;
+switch(empCheck){
+    case 1 : 
+        empHrs = 1;
+        break;
+    case 2:
+        empHrs = 2;
+        break;
+    default:
+        empHrs=0;
 }
-else {
-     console.log("Employee is present");
-}
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Daily Emp Wage :" +empWage);

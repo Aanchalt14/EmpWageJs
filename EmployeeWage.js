679bc5d9ece4f9b0353ecdbd69261cd714cea121
Aanchalt14 <aanchalt71@gmail.com>
2021-02-06 09:27:10 +0530

    Add : Calculate Emp wage using Switch Case

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,21 @@
-console.log('Welcome to EmployeeWage Computation');
-const IS_ABSENT =0
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if ( empCheck == IS_ABSENT) {
-    console.log("Employee is absent");
+const IS_PART_TIME =1;
+const IS_FULL_TIME =2 ;
+const PART_TIME_HOUR=4;
+const FULL_TIME_HOURS =8;
+const WAGE_PER_HOUR=20;
+
+let empHrs=0;
+let empCheck=Math.floor(Math.random()*10)%3;
+switch(empCheck){
+    case 1 : 
+        empHrs = 4;     //part time hour
+        break;
+    case 2:
+        empHrs = 8;     //full time hour
+        break;
+    default:
+        empHrs=0;
 }
-else {
-     console.log("Employee is present");
-}
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage :" +empWage);

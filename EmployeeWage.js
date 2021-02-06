bae0b001ae24e7f8fd2d94725e6f7783adf05665
Aanchalt14 <aanchalt71@gmail.com>
2021-02-06 09:20:00 +0530

    Add : Calculate Part time emp wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,16 @@
-console.log('Welcome to EmployeeWage Computation');
+const FULL_TIME_HOURS = 10;
+const WAGE_PER_HOUR = 20;
+const PART_TIME_HOUR=8;
+
 const IS_ABSENT =0
 let empCheck = Math.floor(Math.random() * 10) % 2;
 if ( empCheck == IS_ABSENT) {
     console.log("Employee is absent");
+    console.log('Part time Emp Wage : 0');
+
 }
 else {
      console.log("Employee is present");
+     let dailyEmpWage = PART_TIME_HOUR * WAGE_PER_HOUR; 
+     console.log("Part time  Emp Wage : " + dailyEmpWage);
 }

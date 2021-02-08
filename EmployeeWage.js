a68df70517d4ff1a72b60813c89666b537056943
Aanchalt14 <aanchalt71@gmail.com>
2021-02-08 18:22:09 +0530

    Add : Wages till condition get satisfied

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,28 @@
-console.log('Welcome to EmployeeWage Computation');
-const IS_ABSENT =0
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if ( empCheck == IS_ABSENT) {
+console.log('Welcome to EmployeeWageComputation !');
+var isPresent = 1;
+var isPartTime =2;
+empRatePerHr = 20;
+var MaxEmpHrs = 100;
+var totalWorkingDays = 20;
+workingDays = 0;
+totalEmpHrs =0;
+while (workingDays < totalWorkingDays && totalEmpHrs <= MaxEmpHrs) {
+var empCheck = Math.floor(Math.random() * 3);
+workingDays++; 
+switch(empCheck){
+ case isPresent :
+    console.log("Employee is present");
+    empHrs = 8;
+    break;
+ case isPartTime :
+    console.log("Employee is part Time");
+    empHrs = 4;
+    break;
+ default :   
     console.log("Employee is absent");
+    empHrs = 0;
 }
-else {
-     console.log("Employee is present");
+ totalEmpHrs += empHrs;
 }
+var salary = empRatePerHr * totalEmpHrs ;
+console.log("Salary = ",salary)

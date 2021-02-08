ca8f79157501290750754a40e2f8651adf41032f
Aanchalt14 <aanchalt71@gmail.com>
2021-02-08 18:27:25 +0530

    REFACTORED : write a function to get work hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,31 @@
-console.log('Welcome to EmployeeWage Computation');
-const IS_ABSENT =0
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if ( empCheck == IS_ABSENT) {
-    console.log("Employee is absent");
+class Employee {
+
+    getWorkHrs(totalWorkingDays, MaxEmpHrs) {
+        var workingDays = 0;
+        var totalEmpHrs = 0;
+        while (workingDays < totalWorkingDays && totalEmpHrs <= MaxEmpHrs) {
+            var isPresent = 1;
+            var isPartTime = 2;
+            var empHrs = 0;
+            var empCheck = Math.floor(Math.random() * 3);
+            workingDays++;
+            switch (empCheck) {
+                case isPresent:
+                    empHrs = 8;
+                    break;
+                case isPartTime:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
+        }
+        return totalEmpHrs;
+    }
 }
-else {
-     console.log("Employee is present");
-}
+empRatePerHr = 20;
+const employeeWage = new Employee();
+var wage = employeeWage.getWorkHrs(20, 100);
+var salary = empRatePerHr * wage;
+console.log("Salary = ", salary)

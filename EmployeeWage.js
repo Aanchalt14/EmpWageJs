678f6c8b56522e82c48ff876f6c7a41b604cb2aa
Aanchalt14 <aanchalt71@gmail.com>
2021-02-08 18:17:42 +0530

    Add : Calculate Emp wage for month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,24 @@
-console.log('Welcome to EmployeeWage Computation');
-const IS_ABSENT =0
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if ( empCheck == IS_ABSENT) {
+console.log('Welcome to EmployeeWageComputation !');
+var isPresent = 1;
+var isPartTime =2;
+empRatePerHr = 20;
+var totalEmpHrs = 0 ;
+for( var i=1;i<=20;i++){
+var empCheck = Math.floor(Math.random() * 3); 
+switch(empCheck){
+ case isPresent :
+    console.log("Employee is present");
+    empHrs = 8;
+    break;
+ case isPartTime :
+    console.log("Employee is part Time");
+    empHrs = 4;
+    break;
+ default :   
     console.log("Employee is absent");
+    empHrs = 0;
 }
-else {
-     console.log("Employee is present");
+ totalEmpHrs += empHrs;
 }
+var salary = empRatePerHr * totalEmpHrs ;
+console.log("Salary = ",salary)
